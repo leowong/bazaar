64e85766b5563b45dbaecc151845d480b7b45cfe
Leo Wong <gnowoel@gmail.com>
2010-05-15 18:23:15 +0800

    fixing dependant issue of products

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   attr_accessible :username, :email, :password, :password_confirmation
   acts_as_authentic
-  has_many :products
+  has_many :products, :dependent => :destroy
 end
