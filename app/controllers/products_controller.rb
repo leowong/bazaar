63729e39c511c6443ba2f5a5170dbcae03b6dc73
Leo Wong <gnowoel@gmail.com>
2010-05-20 02:43:10 +0800

    adding basic sorting support for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,13 @@
 class ProductsController < ApplicationController
   def index
-    @search = Product.name_like_all(params[:search].to_s.split)
-    @products = @search.all
+    @search = Product.name_like_all(params[:q].to_s.split)
+    if params[:order] == "pa"
+      @products = @search.ascend_by_price
+    elsif params[:order] == "pd"
+      @products = @search.descend_by_price
+    else
+      @products = @search.ascend_by_price
+    end
   end
   
   def show
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,19 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def ordering_options
+    [['Price: low to high', 'pa'], ['Price: high to low', 'pd']]
+  end
+
+  def reset_ordering_parameter
+    uri = request.request_uri
+    if uri.index("order=")
+      uri = uri.sub(/&?order=\w*/, '')
+    end
+    if uri.index("?")
+      uri += "&order="
+    else
+      uri += "?order="
+    end
+    uri.sub('?&', '?')
+  end
 end
