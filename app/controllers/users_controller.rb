817e373f29b056dddbf01efda2dcf13910a479da
Leo Wong <gnowoel@gmail.com>
2010-06-08 10:54:06 +0800

    keep improving the look (stores index)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
   def index
     if params[:q] && !params[:q].empty?
-      @search = User.with_role(:seller).username_or_abbreviation_or_store_name_like_all(params[:q].to_s.split)
-      @users = @search.paginate :page => params[:page], :per_page => 10
+      @search = User.with_role(:seller).username_or_abbreviation_or_store_name_or_description_like_all(params[:q].to_s.split)
     else
-      @users = User.with_role(:seller).paginate :page => params[:page], :per_page => 10
+      @search = User.with_role(:seller)
     end
+    @users_count = @search.size
+    @users = @search.paginate :page => params[:page], :per_page => 10 
   end
   
   def show
