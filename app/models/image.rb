241ae4e3313bf87a239441ba8966fbda6b0a2130
Leo Wong <gnowoel@gmail.com>
2010-06-24 16:21:42 +0800

    setting expiry headers for static images in amazon s3
    don't know how to set cache control for amazon cloudfront

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,12 +1,13 @@
 class Image < Asset
   has_attached_file :attachment, 
-                    :storage => ENV['S3_BUCKET'] ? :s3 : :filesystem,
-                    :bucket => ENV['S3_BUCKET'],
-                    :styles => { :mini => '48x48#', :small => '100x100#', :product => '240x240#', :original => '600x600#' }, 
-                    :default_style => :product,
-                    :url => ENV['S3_BUCKET'] ? "assets/products/:id/:style/:basename.:extension" : "/assets/products/:id/:style/:basename.:extension",
-                    :path => ENV['S3_BUCKET'] ? "assets/products/:id/:style/:basename.:extension" : ":rails_root/public/assets/products/:id/:style/:basename.:extension",
-                    :s3_credentials => { :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET'] }
+    :storage => ENV['S3_BUCKET'] ? :s3 : :filesystem,
+    :bucket => ENV['S3_BUCKET'],
+    :styles => { :mini => '48x48#', :small => '100x100#', :product => '240x240#', :original => '600x600#' }, 
+    :default_style => :product,
+    :url => ENV['S3_BUCKET'] ? "assets/products/:id/:style/:basename.:extension" : "/assets/products/:id/:style/:basename.:extension",
+    :path => ENV['S3_BUCKET'] ? "assets/products/:id/:style/:basename.:extension" : ":rails_root/public/assets/products/:id/:style/:basename.:extension",
+    :s3_credentials => { :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET'] },
+    :s3_headers => {'Cache-Control' => 'max-age=315576000', 'Expires' => 10.years.from_now.httpdate }
 
   validates_attachment_presence :attachment
   validates_attachment_size :attachment, :less_than => 5.megabytes
