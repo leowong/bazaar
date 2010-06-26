1a1ebec8998ffd37f90941b77445a77b455cc55b
Leo Wong <gnowoel@gmail.com>
2010-06-27 00:56:19 +0800

    adding zoom effect for product images

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,13 +1,9 @@
 var add_image_handlers = function() {
-  $("#main-image").data('selectedThumb', $('#main-image img').attr('src'));
   $('ul.thumbnails li').eq(0).addClass('selected');
   $('ul.thumbnails li a').click(function() {
-    $("#main-image").data('selectedThumb', $(this).attr('href'));
     $('ul.thumbnails li').removeClass('selected');
     $(this).parent('li').addClass('selected');
     return false;
-  }).click(function() {
-      $('#main-image img').attr('src', $(this).attr('href').replace('mini', 'product'));
   });
 };
 
