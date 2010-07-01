86855bf491077b5fc5c42dd5176cc8beec2781a3
Leo Wong <gnowoel@gmail.com>
2010-07-01 23:32:20 +0800

    moving pages controller to admin namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :pages
-
   map.login "login", :controller => "user_sessions", :action => "new"
   map.logout "logout", :controller => "user_sessions", :action => "destroy"
   map.search 'search', :controller => 'search', :action => 'index'
@@ -10,11 +8,13 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :user_sessions
   map.resources :users, :as => 'stores', :only => [:index, :show]
   map.resources :products, :only => [:index, :show]
+  map.resources :pages, :only => :show
 
   map.namespace :admin do |admin|
     admin.resources :products, :collection => { :sort => :post }
     admin.resources :users, :as => 'stores'
     admin.resources :images, :except => [:index, :show]
+    admin.resources :pages
   end
 
   map.root :controller => "welcome"
