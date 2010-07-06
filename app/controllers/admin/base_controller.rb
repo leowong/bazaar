f08d6bd805888514661cf020d7fb6ce306562889
Leo Wong <gnowoel@gmail.com>
2010-07-06 11:16:04 +0800

    adding admin layout

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,6 +1,8 @@
 class Admin::BaseController < ApplicationController
   before_filter :login_required
 
+  layout "admin"
+
   def current_ability
     @current_ability ||= AdminAbility.new(current_user)
   end
